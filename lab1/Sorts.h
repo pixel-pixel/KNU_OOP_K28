0399d331572371a66bb04cb93be4f8d7949b6fc0
Andrew Bartish <bartishco@gmail.com>
2020-09-14 12:58:21 +0300

    delete something :)

---
+++ lab1/Sorts.h
@@ -0,0 +1,8 @@
+//
+// Created by andrew on 14.09.20.
+//
+
+#ifndef KNU_OOP_K28_SORTS_H
+#define KNU_OOP_K28_SORTS_H
+
+#endif //KNU_OOP_K28_SORTS_H
--- lab1/lab1_main.cpp
+++ lab1/lab1_main.cpp
@@ -7,9 +7,6 @@ using namespace std;
 
 int main() {
     LinkedList<Book> kek;
-    kek.add(new Book("kek", 15, "huinya"));
-    kek.add(new Book("kel", 15, "huinya"));
-    kek.add(new Book("lol", 15, "huinya"));
     kek.remove(-1);
     cout << kek;
 
